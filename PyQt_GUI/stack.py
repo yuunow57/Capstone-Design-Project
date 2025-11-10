c812e295c3b212336fae55aca1976bc98a46948c
wooklee0217 <ssg6989@naver.com>
2025-11-10 19:35:43 +0900

    fix

--- PyQt_GUI/stack.py
+++ PyQt_GUI/stack.py
@@ -46,6 +46,7 @@ class StackApp(QtWidgets.QMainWindow):
 
 
         self.setting_controller = SettingController(page_setting)
+
         
         self.sungp_controller   = MonitoringController(page_sungp, sungp_csv)
         self.battery_controller = MonitoringController(page_battery, battery_csv)
@@ -69,6 +70,7 @@ class StackApp(QtWidgets.QMainWindow):
 
         # 종료 버튼
         self.pushButton_6.clicked.connect(self.close)
+        self.pushButton_6.setStyleSheet("text-align: left; padding-left: 10px; color: #333333;") ## 버튼 6 스타일
 
         # 초기 페이지
         self.change_page(self.pages[0])
@@ -92,10 +94,20 @@ class StackApp(QtWidgets.QMainWindow):
         self.stack.setCurrentWidget(page)
         idx = self.pages.index(page)
         for i, btn in enumerate(self.buttons):
+        # 기존 스타일 가져오기
+            style = btn.styleSheet()
+        # 색상만 바꾸기 위해 기존 style에서 color 제거
+            style = remove_color_from_stylesheet(style)
+          # 색상 추가
             if i == idx:
-                btn.setStyleSheet("color: #9E1010; font-weight: bold;")
+                btn.setStyleSheet(style + "color: #9E1010;")
             else:
-                btn.setStyleSheet("color: #33333; font-weight: normal;")
+                btn.setStyleSheet(style + "color: #333333;")
+
+def remove_color_from_stylesheet(style):
+    import re
+    # 기존 style에 color 속성이 있으면 제거
+    return re.sub(r'color\s*:\s*#[0-9A-Fa-f]+;', '', style)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
